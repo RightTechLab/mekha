ee8d941c5cb54e1adc23edee71cc9abca30be72c
Phoovich <Phoovich@gmail.com>
2025-07-09 00:11:52 +0700

    created HeaderIcon.tsx to display a header bar whit profile icon

---
+++ components/HeaderIcon.tsx
@@ -0,0 +1,35 @@
+import { View, StyleSheet, Pressable } from "react-native";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+
+export default function HeaderIcon() {
+  return (
+    <View style={styles.header}>
+      {/* TODO: impiment onPress anoter */}
+      <Pressable onPress={() => console.log("Menu Pressed")}>
+        <Ionicons name="person-circle-outline" size={32} color="#4B3885" />
+      </Pressable>
+
+      {/* TODO: impiment onPress anoter */}
+      <Pressable onPress={() => console.log("Logo Pressed")}>
+        <MaterialIcons name="menu" size={32} color="#000" />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffc3c3", //  NOTE: delete this after testing
+  },
+  header: {
+    backgroundColor: "#f8f8f8", //  NOTE: delete this after testing
+
+    marginTop: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
