4a2ca1685b4bb4d7682644af27a38c91cbc0bece
Phoovich <Phoovich@gmail.com>
2025-08-16 15:02:55 +0700

    fix: scanner request
    if not allow can go to setting to set allow

--- app/scanner/index.tsx
+++ app/scanner/index.tsx
@@ -1,14 +1,17 @@
 import { CameraView } from "expo-camera";
 import { Stack, router } from "expo-router";
 import React, { useRef, useEffect } from "react";
+import { Button, Text, Linking } from "react-native";
 import { Platform, SafeAreaView, StatusBar, AppState } from "react-native";
 import { useNwcStore } from "@/lib/State/appStore";
 import * as SecureStore from "expo-secure-store";
+import { useCameraPermissions } from "expo-camera";
 
 export default function Scanner() {
   const setNwcUrl = useNwcStore((s) => s.setNwcUrl);
   const qrLock = useRef(false);
   const appState = useRef(AppState.currentState);
+  const [permission, requestPermission] = useCameraPermissions();
 
   useEffect(() => {
     const subscription = AppState.addEventListener("change", (nextAppState) => {
@@ -23,6 +26,29 @@ export default function Scanner() {
 
     return () => subscription.remove();
   }, []);
+
+  if (!permission) {
+    return null;
+  }
+
+  if (!permission.granted) {
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 16,
+        }}
+      >
+        <Stack.Screen options={{ title: "ขอสิทธิ์กล้อง", headerShown: true }} />
+        <Text style={{ fontSize: 16, textAlign: "center", marginBottom: 16 }}>
+          ต้องการสิทธิ์กล้องเพื่อสแกน QR Code
+        </Text>
+        <Button title="อนุญาตการใช้กล้อง" onPress={Linking.openSettings} />
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
